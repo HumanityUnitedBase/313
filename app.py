11b580047f690329063260e7807279d4278697d8
HumanityUnitedBase <65392057+HumanityUnitedBase@users.noreply.github.com>
2020-05-17 16:51:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "HUB" in message:
-                        message = message.replace("HUB", "")
+                    if "Hub!" in message:
+                        message = message.replace("Hub!", " Hub!")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
